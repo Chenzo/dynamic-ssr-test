ec31c3b50d84da38b436090e0980115fe251290c
Chenzorama <chenzorama@hotmail.com>
2023-04-21 09:29:24 -0400

    aaaand it works

---
+++ components/TestCompOne.js
@@ -0,0 +1,9 @@
+
+
+export default function TestCompOne({ children }) {
+  return (
+    <div>
+      <span>I'm Fake Component Number 1!</span>
+    </div>
+  );
+}
---
+++ components/TestCompTwo.js
@@ -0,0 +1,9 @@
+
+
+export default function TestCompTwo({ children }) {
+  return (
+    <div>
+      <span>TWO: I'm comp 2</span>
+    </div>
+  );
+}
